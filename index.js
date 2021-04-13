ab78692c9f6dca51c4a2fecb136564a06848abd8
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-04-13 19:16:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-  txt = document.querySelector("#text");
-  txt.textContent = "This is really cool!";
+  document.querySelector("#text").textContent = "This is really cool!";
 });
